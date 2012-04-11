46704e0f50300a7ed1945eb48439823c9a985e1f
Kristian Freeman <kristianfreeman@gmail.com>
2012-04-11 12:29:37 -0700

    Add new method calls for full version and short name, with corresponding tests

--- lib/which_osx.rb
+++ lib/which_osx.rb
@@ -1,14 +1,44 @@
 # A simple tool to determine the current version of Mac OS X.
-# TODO : Catch if it's *not* OS X - whether that's Windows, or Linux.
+
+# Regex for "##.#.#"
+output = `sw_vers`.scan(/\d\d\.\d\.\d/).flatten.to_s
+
+# Take out some weird bits
+output.delete! "[]\""
+VERSION = output
 
 module WhichOSX
   def self.version
-    output = `sw_vers`.scan(/\d\d\.\d\.\d/).flatten.to_s
-    output.delete! "[]\""
-    return output
+    short_version = VERSION
+    return short_version
     
   rescue Errno::ENOENT
     puts "It looks like you aren't using Mac OS X. Sorry!"
     
+  end
+
+  def self.full_version
+    full_version = "Mac OS X " + VERSION
+    return full_version
+  end
+
+  def self.short_name
+    # We just want the major versions -- 10.6, 10.7, etc.
+    major_version = VERSION.chop.chop
+
+    case
+    when major_version == "10.4"
+      short_name = "Tiger"
+    when major_version == "10.5"
+      short_name = "Leopard"
+    when major_version == "10.6"
+      short_name = "Snow Leopard"
+    when major_version == "10.7"
+      short_name = "Lion"
+    when major_version == "10.8"
+      short_name = "Mountain Lion"
+    end
+
+    return short_name
   end
 end
--- spec/which_osx_spec.rb
+++ spec/which_osx_spec.rb
@@ -1,4 +1,6 @@
 # which_osx_spec.rb
+# todo: mock this so we don't encounter errors when we update our OS
+
 require 'which_osx'
 
   describe "version" do
@@ -13,5 +15,21 @@ require 'which_osx'
     # it "should tell you when you're not on Mac OS X" do
     #   # how best to mock a non-Mac OS X environment?
     # end
-
   end
+
+  describe "full version" do
+    it "should return the full name of the OS" do
+      WhichOSX.full_version.should === "Mac OS X 10.7.3"
+    end
+  end
+
+  describe "short name" do
+    it "should chomp the version into a major release number" do
+      WhichOSX.version.chop.chop.should === "10.7"
+    end
+
+    it "should return a major release name (Lion, Leopard, etc.)" do
+      WhichOSX.short_name.should === "Lion"
+    end
+  end
+
